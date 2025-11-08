59690d834fe5bd65ced774045bdede7b8ba73646
Bari <alex.c9777700@gmail.com>
2025-11-08 12:04:13 +0100

    Bump to Core 7.0.3

---
+++ WorldOfWarcraft.Tests/FakeDataset.cs
@@ -0,0 +1,57 @@
+﻿using GamersCommunity.Core.Tests;
+using Microsoft.EntityFrameworkCore;
+using WorldOfWarcraft.Database.Context;
+using WorldOfWarcraft.Database.Models;
+
+namespace WorldOfWarcraft.Tests
+{
+    public class FakeDataset : IFakeDataset<WorldOfWarcraftDbContext>
+    {
+        /// <summary>
+        /// Creates a new in-memory <see cref="WorldOfWarcraftDbContext"/> with a unique database name.
+        /// </summary>
+        public WorldOfWarcraftDbContext CreateFakeContext() => SeedFakeData(new WorldOfWarcraftDbContext(
+            new DbContextOptionsBuilder<WorldOfWarcraftDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options
+            ));
+
+        /// <summary>
+        /// Seeds the full test dataset required across all services (relations, dependencies, etc.).
+        /// </summary>
+        private WorldOfWarcraftDbContext SeedFakeData(WorldOfWarcraftDbContext ctx)
+        {
+            ctx.ChangeTracker.AutoDetectChangesEnabled = false;
+
+            #region Classes
+
+            if (!ctx.Classes.Any())
+            {
+                ctx.Classes.AddRange(
+                    new Class
+                    {
+                        Id = 1,
+                        Entitled = "Class A",
+                        CreationDate = DateTime.UtcNow,
+                        ModificationDate = DateTime.UtcNow,
+                    },
+                    new Class
+                    {
+                        Id = 2,
+                        Entitled = "Class B",
+                        CreationDate = DateTime.UtcNow,
+                        ModificationDate = DateTime.UtcNow
+                    }
+                );
+            }
+
+            #endregion
+
+            ctx.ChangeTracker.AutoDetectChangesEnabled = true;
+
+            ctx.SaveChanges();
+
+            return ctx;
+        }
+    }
+}
---
+++ WorldOfWarcraft.Tests/Services/Data/ClassesServiceTests.cs
@@ -0,0 +1,21 @@
+﻿using GamersCommunity.Core.Tests;
+using WorldOfWarcraft.Consumer.Services.Data;
+using WorldOfWarcraft.Database.Context;
+using WorldOfWarcraft.Database.Models;
+
+namespace WorldOfWarcraft.Tests.Services.Data
+{
+    public class ClassesServiceTests : GenericDataServiceTests<WorldOfWarcraftDbContext, ClassesService, Class>, IClassFixture<FakeDataset>
+    {
+        protected override List<Class> GetFakeData() => [];
+
+        protected override Class GetNewEntity() => new()
+        {
+            Entitled = "New class",
+            CreationDate = DateTime.UtcNow,
+            ModificationDate = DateTime.UtcNow,
+        };
+
+        protected override ClassesService CreateService() => new(CreateContext());
+    }
+}
